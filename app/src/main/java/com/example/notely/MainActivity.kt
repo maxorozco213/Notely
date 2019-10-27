3a8a530d72fa7b6fa551d6eaa44ea0444b3a4a9a
Max <maxorozco213@gmail.com>
2019-10-27 16:26:06 -0700

    App askes for write permissions upon launch

--- app/src/main/java/com/example/notely/MainActivity.kt
+++ app/src/main/java/com/example/notely/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.notely
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +13,51 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("Testing")
+        print(ContextCompat.checkSelfPermission(this,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED)
+        this.getPermissions()
+    }
+
+    private fun getPermissions() {
+        if(ContextCompat.checkSelfPermission(this,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                print("App needs to save photos to your local storage")
+            } else {
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    1)
+                print("Permissions have been granted.")
+            }
+        } else {
+            print("Permissions have already been granted.")
+        }
+    }
+
+    //Not currently being used
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            1 -> {
+                if((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    print("Save the photo")
+                } else {
+                    print("Poops can't save the photo")
+                }
+
+                return
+            }
+
+            else -> {
+                print("Ignoring requests ")
+            }
+        }
     }
 }
