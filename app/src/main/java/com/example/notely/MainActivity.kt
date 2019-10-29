2a8d38b06a53a0a39c1058eb1de85597a0cfe80a
Max <maxorozco213@gmail.com>
2019-10-29 16:05:11 -0700

    App detects and asks for permissions if needed

--- app/src/main/java/com/example/notely/MainActivity.kt
+++ app/src/main/java/com/example/notely/MainActivity.kt
@@ -1,62 +1,69 @@
 package com.example.notely
 
+import android.app.Dialog
+import android.content.DialogInterface
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+    private val STORAGE_PERMISSION_CODE: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("Testing")
-        print(ContextCompat.checkSelfPermission(this,
-            android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED)
-        this.getPermissions()
+
+        val requestPermissions: Button  = findViewById(R.id.permissionButton)
+        requestPermissions.setOnClickListener{this.checkPermissions()}
     }
 
-    private fun getPermissions() {
-        if(ContextCompat.checkSelfPermission(this,
-            android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-            != PackageManager.PERMISSION_GRANTED) {
-
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                print("App needs to save photos to your local storage")
-            } else {
-                ActivityCompat.requestPermissions(this,
-                    arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                    1)
-                print("Permissions have been granted.")
-            }
+    private fun checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this, "Permissions already granted", Toast.LENGTH_SHORT).show()
         } else {
-            print("Permissions have already been granted.")
+            this.requestPermissions()
         }
     }
 
-    //Not currently being used
+    private fun requestPermissions() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            AlertDialog.Builder(this)
+                .setTitle("Permission needed")
+                .setMessage("This is needed")
+                .setPositiveButton("Ok") { DialogInterface, which ->
+                    Log.i("Permissions", "User pressed OK")
+                }
+                .setNegativeButton("Cancel") { DialogInterface, which ->
+                    Log.i("Permissions", "Storage permission denied")
+                }
+                .show()
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), STORAGE_PERMISSION_CODE)
+        }
+    }
+
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        when (requestCode) {
-            1 -> {
-                if((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    print("Save the photo")
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            STORAGE_PERMISSION_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+
+                    Log.i("", "Permission has been denied by user")
                 } else {
-                    print("Poops can't save the photo")
+                    Log.i("", "Permission has been granted by user")
                 }
-
-                return
-            }
-
-            else -> {
-                print("Ignoring requests ")
             }
         }
     }
