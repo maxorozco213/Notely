b237152fb0a9b74e17ee1f321813d24c0d6fe315
Urbanham <Urbanham15@gmail.com>
2019-12-07 13:46:21 -0800

    Files tab shows its empty now

--- app/src/main/java/com/example/notely/ui/files/FilesFragment.kt
+++ app/src/main/java/com/example/notely/ui/files/FilesFragment.kt
@@ -15,11 +15,30 @@ import com.example.notely.databinding.FragmentFilesBinding
 class FilesFragment : Fragment() {
     private lateinit var filesViewModel: FilesViewModel
 
+    companion object {
+        private const val ARG_PATH: String = "com.example.notely.fileslist.path"
+        fun build(block: Builder.() -> Unit) = Builder().apply(block).build()
+    }
+
+    class Builder {
+        var path: String = ""
+
+        fun build(): FilesFragment {
+            val fragment = FilesFragment()
+            val args = Bundle()
+            args.putString(ARG_PATH, path)
+            fragment.arguments = args;
+            return fragment
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
         filesViewModel =
             ViewModelProviders.of(this).get(FilesViewModel::class.java)
 
@@ -27,7 +46,7 @@ class FilesFragment : Fragment() {
             inflater, R.layout.fragment_files, container, false)
         binding.files = this
 
-        val textView: TextView = binding.textFiles
+        val textView: TextView = binding.textNothing
 
         filesViewModel.text.observe(this, Observer {
             textView.text = it
