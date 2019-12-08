de1023dbd9d83c0b2817f06e66fb4aefad582500
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2019-12-08 15:53:26 -0800

    added more styling and formatting to fragments

--- app/src/main/java/com/example/notely/ui/user/UserFragment.kt
+++ app/src/main/java/com/example/notely/ui/user/UserFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -29,10 +28,6 @@ class UserFragment : Fragment() {
             inflater, R.layout.fragment_user, container, false)
         binding.user = this
 
-        val textView: TextView = binding.textUser
-        userViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
         userViewModel.user.observe(this, Observer {
             if ( it == null ) {
                 findNavController().navigate(R.id.action_navigation_user_to_navigation_login)
